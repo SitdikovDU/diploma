034543ddc0442ac489235b45899a8ea1f68f9467
SitdikovDU <71568127+SitdikovDU@users.noreply.github.com>
2024-05-03 19:18:18 +0300

    Update em.py

--- em.py
+++ em.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy.stats import multivariate_normal
-from generations import rotate_matrix
+from scipy.stats import chi2
+from scipy.special import comb
 import random
 
 
 def rotate_matrix(matrix, phi):
     phi = 2*np.pi/360*phi
     rotation_matrix = np.array([[np.cos(phi), -np.sin(phi)], [np.sin(phi), np.cos(phi)]])
-
     return rotation_matrix @ matrix @ rotation_matrix.T
 
 class EM:
@@ -29,12 +29,30 @@ class EM:
     def get_cluster_type(self, k):
          return np.argmax(self.cov_weights_[k])
         
+    def get_cluster_info(self, k):
+        mu = self.means_[k]
+        cluster_type = self.get_cluster_type(k)
+        angle = self.phi[k, cluster_type]
+        cov = self.covariances_[self.get_cluster_type(k)]
+
+        return mu, rotate_matrix(cov, angle)
+
+    def aic(self):
+        k = (self.n_components - 1) + 2 * (self.n_components - self.fixed_means) + len(self.covariances_) * self.n_components + self.n_components
+
+        return 2*k - 2 * self.log_likelihood()
+
+    def bic(self):
+        k = (self.n_components - 1) + 2 * (self.n_components - self.fixed_means) + len(self.covariances_) * self.n_components + self.n_components
+
+        return k* np.log(len(self.data)) - 2 * self.log_likelihood()
+
     def log_likelihood(self):
         num_samples = len(self.data)
         ll = 0
         labels, covs = self.predict()
         for i in range(num_samples):
-            value = self.weights_[labels[i]] * self.cov_weights_[labels[i], covs[i]]* multivariate_normal.pdf(self.data[i], mean=self.means_[labels[i]], cov=rotate_matrix(self.covariances_[covs[i]], self.phi[labels[i],covs[i]]))
+            value = self.weights_[labels[i]] * self.cov_weights_[labels[i], covs[i]] * multivariate_normal.pdf(self.data[i], mean=self.means_[labels[i]], cov=rotate_matrix(self.covariances_[covs[i]], self.phi[labels[i],covs[i]]))
             ll += np.log(value + self.eps)
         return ll
 
@@ -48,8 +66,10 @@ class EM:
             for j in range(n_covariances):
                 responsibilities[:, k, j] = self.weights_[k] * self.cov_weights_[k, j] * multivariate_normal.pdf(self.data, mean=self.means_[k], cov=rotate_matrix(self.covariances_[j], self.phi[k, j]))
         
-        for i in range(num_samples):   
-            responsibilities[i, :, :] /= self.eps + np.sum(responsibilities[i, :, :])
+        #for i in range(num_samples):   
+        #    responsibilities[i, :, :] /= self.eps + np.sum(responsibilities[i, :, :])
+        sum_responsibilities = np.sum(responsibilities, axis=(1, 2), keepdims=True)
+        responsibilities /= self.eps + sum_responsibilities
         
         return responsibilities
 
@@ -60,16 +80,16 @@ class EM:
         self.weights_ = np.sum(responsibilities, axis=(0,2)) / num_samples
         self.cov_weights_ = np.divide(np.sum(responsibilities, axis=0), np.sum(responsibilities, axis=(0, 2))[:, np.newaxis] + self.eps)
 
-        for l in range(self.fixed_means, self.n_components):
-            c = self.eps * np.ones((2, 1))
-            denominator = self.eps
-            for j in range(n_covariances):
-                for i in range(num_samples):
-                    c += np.linalg.inv(self.covariances_[j]) @ self.data[i].reshape((-1, 1)) *  responsibilities[i, l, j]
-                
-                denominator += np.linalg.inv(self.covariances_[j]) * np.sum(responsibilities[:, l, j])
+        point_probs = np.sum(responsibilities, axis=2)
+        point_labels = np.argmax(point_probs, axis=1)
 
-            self.means_[l] = np.array(np.linalg.inv(denominator) @ c).flatten()
+        for k in range(self.fixed_means, self.n_components):
+            indices = np.where(point_labels == k)
+            if np.sum(point_probs[indices, k]) < 1e-50:
+                self.means_[k] = self.data[np.argmin(self.density)]
+                break
+            else:
+                self.means_[k] = point_probs[indices, k] @ self.data[indices] / np.sum(point_probs[indices, k])
 
 
         for k in range(self.n_components):
@@ -199,3 +219,85 @@ class multiEM(EM):
         self.cov_weights_ = self.best_attempt["cov_weights"]
         self.phi = self.best_attempt["phi"]
         self.means_ = self.best_attempt["means"]
+
+
+
+
+class iterativeEM(EM):
+    def __init__(self, max_iter=100, reg_covar=1e-6, eps=1e-312, min_center_distance=0.01):
+        self.n_components = 0
+        self.max_iter = max_iter
+        self.reg_covar = reg_covar
+        self.eps = eps
+        self.best_attempt = {"ll": -np.inf}
+        self.history = []
+        self.min_center_distance = min_center_distance
+        
+    def check_outliers(self, points, k, p0=0.95, alpha=0.01):
+        n = len(points)
+        mu, rotated_cov = self.get_cluster_info(k) 
+        
+        if p0 * n < 5:
+            return 1, False
+
+        quantile95 = chi2.ppf(0.95, 2)
+        k = sum((point - mu) @ np.linalg.inv(rotated_cov) @ (point - mu) < quantile95 for point in points)
+        p_value = sum([comb(n, i) * p0**i *(1- p0)**(n-i) for i in range(k+1)])
+        
+        return p_value, p_value < alpha
+        
+    def fit(self, data, fixed_means=None, means=None, weights=None, covariances=None, verbose=0, n_init=10, alpha=1e-3):
+        if fixed_means is not None and not self.n_components:
+            self.n_components = len(fixed_means) - 1
+            
+        while True:
+            self.best_attempt["ll"] = -np.inf
+            self.n_components += 1
+
+            for i in range(n_init):
+                super().__init__(n_components=self.n_components, max_iter=self.max_iter, reg_covar=self.reg_covar, eps=self.eps)
+                super().fit(data, fixed_means=fixed_means, means=means, weights=weights, covariances=covariances, verbose=verbose % 2)
+
+                ll = super().log_likelihood()
+                self.history.append(ll)
+                
+                if self.best_attempt["ll"] < ll:
+                    self.best_attempt["weights"] = self.weights_
+                    self.best_attempt["cov_weights"] = self.cov_weights_
+                    self.best_attempt["phi"] = self.phi
+                    self.best_attempt["means"] = self.means_
+                    self.best_attempt["ll"] = ll
+
+            found = False
+            min_p_value = 1
+            
+            labels, covs = self.predict()
+            for k in range(self.n_components):
+                inds = np.where(np.array(labels) == k)[0]
+                cluster_points = data[inds]
+                p_value, test = self.check_outliers(cluster_points, k, alpha=alpha)                
+                if test:
+                    density = self.cluster_density(cluster_points, k)
+                    new_center = np.argmin(density)
+                    
+                    if p_value < min_p_value:
+                        means = np.vstack([self.means_, cluster_points[new_center]])
+                        min_p_value = p_value
+                    found = True
+            
+            if not found:
+                print(f"stopped with n_components = {self.n_components}")
+                break
+            #print(f"found with p-value = {min_p_value}, n_comp = {self.n_components}")
+            
+            if verbose:
+                print(f"число компонент {self.n_components}, ll = {round(self.best_attempt['ll'], 2)}, aic = {round(self.aic(), 2)}, bic = {round(self.bic(), 2)}")
+            if np.isneginf(self.best_attempt["ll"]):
+                print("stopped, число компонент: ", self.n_components - 1)
+                break
+            else:
+                self.weights_ = self.best_attempt["weights"]
+                self.cov_weights_ = self.best_attempt["cov_weights"]
+                self.phi = self.best_attempt["phi"]
+                self.means_ = self.best_attempt["means"]
+        
